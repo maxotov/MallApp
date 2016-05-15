190a05ef5995bad529d64262d0005d2dfa331709
AbcKaz <berik2031@gmail.com>
2016-05-15 14:28:18 +0600

    Rejected commit: Default

--- app/src/main/java/kz/itdamu/mallapp/entity/Goods.java
+++ app/src/main/java/kz/itdamu/mallapp/entity/Goods.java
@@ -1,25 +1,38 @@
 package kz.itdamu.mallapp.entity;
 
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * Created by Aibol on 05.04.2016.
  */
-public class Goods {
+public class Goods implements Serializable {
+    public static final String PATH = "http://www.villopim.com.br/android/glide/__w-395-593-790-1185__/";
+    public static final String SER_KEY = "GoodsSer";
     private int id;
     private String name;
-    private int price;
+    private double price;
     private String description;
     private int shop_id;
     private String c_date;
     private String m_date;
+    private List<Photo> photoList;
     private int view;
     private int share;
+    private int discount;
+    public Goods(){}
 
-    public int getId() {
-        return id;
+    public Goods(String name,int id) {
+        this.name = name;
+        this.id = id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public List<Photo> getPhotoList() {
+        return photoList;
+    }
+
+    public void setPhotoList(List<Photo> photoList) {
+        this.photoList = photoList;
     }
 
     public String getName() {
@@ -30,11 +43,11 @@ public class Goods {
         this.name = name;
     }
 
-    public int getPrice() {
+    public double getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
+    public void setPrice(double price) {
         this.price = price;
     }
 
@@ -70,12 +83,20 @@ public class Goods {
         this.m_date = m_date;
     }
 
-    public int getView() {
-        return view;
+    public int getId() {
+        return id;
     }
 
-    public void setView(int view) {
-        this.view = view;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public int getDiscount() {
+        return discount;
     }
 
     public int getShare() {
@@ -84,5 +105,13 @@ public class Goods {
 
     public void setShare(int share) {
         this.share = share;
+    }
+
+    public int getView() {
+        return view;
+    }
+
+    public void setView(int view) {
+        this.view = view;
     }
 }
